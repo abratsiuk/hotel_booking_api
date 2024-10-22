3bc945722c7615397b4a4825edea8948a6d9a7f6
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2024-10-22 13:50:15 +0300

    initial project ASP.NET Core Web API with Hotel model and controller, migrated to local database

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using hotel_booking_api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace hotel_booking_api.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+        // Другие DbSet...
+    }
+}
---
+++ Models/Hotel.cs
@@ -0,0 +1,9 @@
+﻿namespace hotel_booking_api.Models
+{
+    public class Hotel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+    }
+}
